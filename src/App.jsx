1b82989c8171670b104a0a587cce01037e36ed1d
Aravind Guggilla <aravindguggilla11@gmail.com>
2026-06-20 11:17:59 +0530

    Implemented ProtectedRoute component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
 import Login from './components/Login'
-
-
+import Home from './components/Home'
+import ReferralDetails from './components/ReferralDetails'
+import NotFound from './components/NotFound'
 import './index.css'
 
-const App = () => <Login />
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/referral/:id"
+          element={
+            <ProtectedRoute>
+              <ReferralDetails />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/referrals"
+          element={
+            <ProtectedRoute>
+              <Navigate to="/" replace />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 export default App
-
